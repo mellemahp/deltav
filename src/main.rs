e6d28d304e6cc95ed7be163120d3e2dcee51987c
Hunter Mellema <mellemahp@gmail.com>
2018-03-27 15:18:32 -0700

    Added Matching of Input parameters to get orbit

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,11 @@ use structopt::StructOpt;
 struct Opt {
     #[structopt(name = "periapsis",
                 short = "p1",
+
+                // DEBUG START
+                default_value = "3.9",
+                // DEBUG END
+
                 help ="Periapsis of the initial Orbit (default units km)")]
     peri: Option<f32>,
     #[structopt(name = "apoapsis",
@@ -16,57 +21,104 @@ struct Opt {
     apo: Option<f32>,
     #[structopt(name = "eccentricity",
                 long = "e",
-                default_value = "0",
+
+                // DEBUG START
+                default_value = "0.0",
+                // DEBUG END
+
                 help ="Eccentricty of the initial Orbit (default units km)")]
-    ecc: f32,
+    ecc: Option<f32>,
     #[structopt(name = "periapsis velocity",
-                long = "vp",
+                long = "vp1",
                 help ="Velocity of orbit at periapsis (default units km/s)")]
-    vel_peri: Option<f32>,
+    vel_peri1: Option<f32>,
     #[structopt(name = "apoapsis velocity",
-                long = "va",
+                long = "va1",
                 help ="Periapsis of orbit at apoapsis (default units km/s)")]
-    vel_apo: Option<f32>,
+    vel_apo1: Option<f32>,
+
+
     #[structopt(name = "central body",
                 long = "cb",
                 default_value = "3",
                 help ="central body orbit is defined around (horizons ID)")]
     spk_id: i32,
-    // #[structopt(subcommand)]
-    // dist_unit: Option<Units>,
-
-}
-/*
-#[derive(StructOpt)]
-enum Units {
-    #[structopt(short = "km")]
-    kilometers,
-    #[structopt(short = "m")]
-    meters,
-    #[structopt(short = "mi")]
-    miles,
-    #[structopt(short = "au")]
-    astro_unit,
 }
 
-#[derive(StructOpt)]
-enum VelocityUnit {
-    #[structopt(short = "m/s")]
-    m_sec,
-    #[structopt(short = "ft/s")]
-    ft_sec,
-    #[structopt(short = "km/s")]
-    km_sec,
-    #[structopt(short = "mi/s")]
-    mi_sec,
-    #[structopt(short = "km/hr")]
-    km_hr,
-    #[structopt(short = "mi/hr")]
-    mi_hr,
+#[derive(Debug)]
+struct Orbit {
+    apoapsis: f32,
+    periapsis: f32,
 }
- */
+
+
+fn angular_momentum(mu: f32, rp: f32, ra: f32) -> f32 {
+    let h = (2.0 * mu).sqrt() * ((rp * ra) / (rp + ra)).sqrt();
+    h
+}
+
+
+fn args_to_orbs(opt: Opt) -> Orbit {
+    match opt {
+        Opt{ peri: Some(_), apo: Some(_), ..} => {
+            let orbit1 = Orbit{
+                apoapsis: opt.apo.unwrap(),
+                periapsis: opt.peri.unwrap(),
+            };
+            return orbit1;
+        },
+        Opt {peri: Some(_), ecc: Some(_), ..} => {
+            let orbit1 = Orbit{
+                apoapsis: (opt.peri.unwrap() * (opt.ecc.unwrap() + 1.0) /
+                           (opt.ecc.unwrap() -1.0 )).abs(),
+                periapsis: opt.peri.unwrap()
+            };
+            return orbit1;
+        },
+        Opt {peri: Some(_), vel_peri1: Some(_), .. } => {
+            let orbit1 = Orbit{
+                // PLACEHOLDER VALUE START
+                apoapsis: 3.5,
+                // PLACEHOLDER VALUE END
+                periapsis: opt.peri.unwrap(),
+            };
+            return orbit1;
+        },
+        Opt {apo: Some(_), ecc: Some(_), ..} => {
+            let orbit1 = Orbit{
+                apoapsis: opt.apo.unwrap(),
+                periapsis: (opt.apo.unwrap() * ( 1.0 - opt.ecc.unwrap()) /
+                            (opt.ecc.unwrap() + 1.0)).abs()
+            };
+            return orbit1;
+        },
+        Opt {apo: Some(_), vel_apo1: Some(_), .. } => {
+            let orbit1 = Orbit{
+                // PLACEHOLDER VALUE START
+                apoapsis: opt.apo.unwrap(),
+                // PLACEHOLDER VALUE END
+                periapsis: 3.5,
+            };
+            return orbit1;
+        },
+
+       Opt {apo: Some(_), vel_peri1: Some(_), .. } |
+       Opt {ecc: Some(_), vel_peri1: Some(_), .. } |
+       Opt {ecc: Some(_), vel_apo1: Some(_), .. } |
+       Opt {peri: Some(_), vel_apo1: Some(_), .. } => {
+            panic!("The arguments you entered are incompatible!");
+       }
+        _ => panic!("Not Enough Arguments! (need at least 2!)")
+    }
+}
+
 
 fn main() {
     let opt = Opt::from_args();
-    println!("{:?}", opt);
+    println!("{:?}",
+             opt);
+   //  let h = angular_momentum(2.0,1.0,2.0);
+    let orbit1 = args_to_orbs(opt);
+    println!("Orbit1: {:?}", orbit1);
+
 }
--- target/debug/deltav.d
+++ target/debug/deltav.d
@@ -1,1 +1,1 @@
-/home/mellemahp/rust_projects/deltav/target/debug/deltav: /home/mellemahp/rust_projects/deltav/src/lib.rs /home/mellemahp/rust_projects/deltav/src/main.rs
+/home/mellemahp/rust_projects/deltav/target/debug/deltav: /home/mellemahp/rust_projects/deltav/src/main.rs /home/mellemahp/rust_projects/deltav/src/lib.rs
